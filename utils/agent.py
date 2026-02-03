c9b48e5d0b7b9edb6315ee50062537491495cf52
cho-jang-hyun <jjh7632016@khu.ac.kr>
2026-02-03 11:37:59 +0900

    Edit penalty square

--- utils/agent.py
+++ utils/agent.py
@@ -586,7 +586,7 @@ class Agent:
         else:
             # Calculate overlap ratio (how much of current sensing area overlaps with others)
             overlap_area_size = current_free_area_size - current_free_area_not_scanned_size
-            overlap_penalty = np.square(overlap_area_size / current_free_area_size)
+            overlap_penalty = overlap_area_size / current_free_area_sizes
 
         return overlap_penalty
         
