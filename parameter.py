8305d6a1e5c803c37f562b13d1e79c1295f982aa
Jimmy291 <jimmywm291@outlook.com>
2026-04-30 19:35:22 +0800

    Update params

--- parameter.py
+++ parameter.py
@@ -18,7 +18,7 @@ Key configurations include:
 - GPU and logging options
 """
 
-FOLDER_NAME = '4_30'
+FOLDER_NAME = '4_30_no_merged_map_albation'
 LOAD_FOLDER_NAME = '4_22_Dual_critic_estimation_metric_with_indv_complete_reward'
 model_path = f'model/{FOLDER_NAME}' # save checkpoint
 load_path = f'load_model/{LOAD_FOLDER_NAME}' # load checkpoint
@@ -84,11 +84,11 @@ MIN_BUDGET = BUDGET_END
 MAX_BUDGET = BUDGET_START
 REPLAY_SIZE = 10000
 MINIMUM_BUFFER_SIZE = 8000
-BATCH_SIZE = 128
+BATCH_SIZE = 256
 LR = 2e-5
 GAMMA = 0.99
 TAU = 0.003  # Soft update coefficient for target network (0.001 ~ 0.01)
-NUM_META_AGENT = 15
+NUM_META_AGENT = 27
 POST_PHASE_REPLAY_MIN_SAMPLES = 512
 POST_PHASE_BATCH_RATIO = 0.3
 
